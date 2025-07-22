35bee6daf714def4f240216d5bca827add1909bf
amazighii <abdessamadmazighi123@gmail.com>
2025-07-22 23:51:54 +0100

    completing mv

--- simple-shell/src/commands/mv.rs
+++ simple-shell/src/commands/mv.rs
@@ -1,5 +1,6 @@
-use std::{env::current_dir, fs::rename};
+use std::io::ErrorKind;
 use std::path::Path;
+use std::{env::current_dir, fs::rename};
 
 pub fn mv(argument: Vec<&str>) {
     if argument.is_empty() {
@@ -17,28 +18,67 @@ pub fn mv(argument: Vec<&str>) {
             break;
         }
 
-        let mut to = format!("{}/{}", dis, v);
+        //still need to handle the dot case
+        // if dis == "." {
+        //     match current_dir() {
+        //         Ok(p) => {
+        //             let path = Path::new(&p);
 
-        //still need to handl the dot case
-        if dis == "." {
-            match current_dir() {
-                Ok(p) => {
-                    let path = Path::new(&p);
+        //             match path.parent() {
+        //                 Some(parent) => to = format!("{}", parent.display()),
+        //                 None => println!("No parent directory"),
+        //             }
+        //         },
+        //         Err(err) => println!("Error: {}", err),
+        //     }
+        // }
 
-                    match path.parent() {
-                        Some(parent) => to = format!("{}", parent.display()),
-                        None => println!("No parent directory"),
-                    }
-                },
-                Err(err) => println!("Error: {}", err),
-            }
-        }
+        let  to = format!("{}/{}", dis, v);
 
-        println!("v: {v}, to: {to}");
-        match rename(v, to) {
+        // println!("v: {v}, to: {to}");
+        // println!("v: {v}, dis: {dis}");
+        match rename(v, &to) {
             Ok(_) => (),
-            Err(err) => println!("Error: {}", err),
+            Err(_) => {
+                let vv = Path::new(v);
+                let to_to = Path::new(&dis);
+                // println!("exists: vv: {}, to_to: {}", vv.exists(), to_to.exists());
+
+                if vv.exists() && to_to.exists() {
+                    if vv.is_file() && to_to.is_file() {
+                        match rename(v, dis) {
+                            Ok(_) => (),
+                            Err(err) => error(err.kind()),
+                        }
+                    } else {
+                        println!(
+                            "mv: cannot overwrite non-directory '{}' with directory '{}' ",
+                            dis, v
+                        );
+                    }
+                } else if !to_to.exists() {
+                        match rename(v, dis) {
+                            Ok(_) => (),
+                            Err(err) => error(err.kind()),
+                        }
+                } else {
+                    println!("mv: source file not found");
+                }
+            }
         }
     }
 }
 
+fn error(err: ErrorKind) {
+    use std::io::ErrorKind;
+
+    match err {
+        ErrorKind::NotFound => eprintln!("mv: source file not found"),
+        ErrorKind::PermissionDenied => eprintln!("mv: permission denied"),
+        ErrorKind::AlreadyExists => eprintln!("mv: destination already exists"),
+        ErrorKind::InvalidInput => eprintln!("mv: invalid input"),
+        _ => eprintln!("mv: unknown error: {err}"),
+    }
+}
+// Not working:
+//mv test2 test1/test2
